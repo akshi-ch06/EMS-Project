084c4fbc8c5687a68c53cb20e03a9a7cfcf33b32
Akshita <akshi.chauhan06@gmail.com>
2026-02-21 12:37:03 +0530

    made small change

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import AuthProvider from "./providers/authProvider";
+import AuthProvider from "./providers/AuthProvider";
 
 createRoot(document.getElementById("root")!).render(
     <AuthProvider>
